f8f9da0651ae17e02767ee23ea01dd06e0b4e03a
Benjamin LEVESQUE <14175665+benjlevesque@users.noreply.github.com>
2020-04-15 17:17:50 +0200

    signatureProvider metamask cancelRequest

--- packages/shared/src/helpers/cancelRequest.ts
+++ packages/shared/src/helpers/cancelRequest.ts
@@ -11,6 +11,17 @@ export const cancelRequest = async (
   network: string | number
 ) => {
   network = chainIdToName(network);
+  const win = window as any;
+  if (!win.ethereum) {
+    throw new Error("ethereum not detected");
+  }
+  let signatureProvider = new CustomSignatureProvider(
+    new Web3Provider((window as any).ethereum).getSigner()
+  );
+  if (!win.ethereum.isMetamask) {
+    const { Web3SignatureProvider } = require("@requestnetwork/web3-signature");
+    signatureProvider = new Web3SignatureProvider(win.ethereum);
+  }
   const rn = new RequestNetwork({
     nodeConnectionConfig: {
       baseURL:
@@ -18,9 +29,7 @@ export const cancelRequest = async (
           ? "https://gateway-rinkeby.request.network"
           : "https://gateway.request.network",
     },
-    signatureProvider: new CustomSignatureProvider(
-      new Web3Provider((window as any).ethereum).getSigner()
-    ),
+    signatureProvider,
   });
 
   const request = await rn.fromRequestId(requestId);
@@ -28,5 +37,5 @@ export const cancelRequest = async (
     type: IdentityTypes.TYPE.ETHEREUM_ADDRESS,
     value: account,
   });
-  await new Promise((resolve) => cancellation.on("confirmed", () => resolve()));
+  await new Promise(resolve => cancellation.on("confirmed", () => resolve()));
 };

a139d3ae388a7af13eca84f7b6f14c9680e28312
PijiDev <pijidev@gmail.com>
2023-01-02 08:12:20 +0100

    Fix: improve hover text 'unknown' for better UX.

--- packages/request-ui/src/RStatusBadge.tsx
+++ packages/request-ui/src/RStatusBadge.tsx
@@ -24,7 +24,7 @@ export const statusLabels: Record<RequestStatus, string> = {
   pending: 'Pending',
   canceled: 'Canceled',
   overpaid: 'Overpaid',
-  unknown: 'Unknown',
+  waiting: 'Waiting',
 };
 
 export const RStatusBadge = (props: IProps) => {
@@ -40,8 +40,8 @@ export const RStatusBadge = (props: IProps) => {
       <div
         className={[classes.status, props.className].join(' ')}
         title={
-          props.status === 'unknown'
-            ? 'The balance computation might have failed'
+          props.status === 'waiting'
+            ? `The request hasn't been indexed by IPFS yet. Please check back later`
             : ''
         }
       >
